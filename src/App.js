6c968682b6ca3d319a1e00036cbee3d4e04500a2
HeyTiina <130788330+HeyTiina@users.noreply.github.com>
2024-07-01 18:00:31 +0200

    name changed to weather

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 
 import "./App.css";
 
-import SearchEngine from "./SearchEngine";
+import Weather from "./Weather.js";
 import Footer from "./Footer.js";
 
 export default function App() {
   return (
     <body>
       <div class="weather-app">
-        <SearchEngine />
+        <Weather />
         <Footer />
       </div>
     </body>
